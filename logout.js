9d655924cda304039a74f50b0bb61cd4fc882e4b
nirajt03 <71938120+nirajt03@users.noreply.github.com>
2023-09-05 00:18:02 +0530

    Update logout.js

--- logout.js
+++ logout.js
@@ -2,7 +2,7 @@ function logout() {
   const response = confirm("Are you sure you want to log out?");
 
   if (response) {
-    window.location.href = "./LoginPage.html";
+    window.location.href = "https://nirajt03.github.io/sample-website/";
     console.log("Ok was pressed");
   } else {
     console.log("Cancel was pressed");
